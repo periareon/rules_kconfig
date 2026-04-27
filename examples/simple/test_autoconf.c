c54816acef6da56bf76e685052a92442c9279612
Andre Brisco <andre.brisco@protonmail.com>
2026-04-26 22:29:15 -0700

    Fix hex values not rendering

--- examples/simple/test_autoconf.c
+++ examples/simple/test_autoconf.c
@@ -1,3 +1,11 @@
 #include "config.h"
 
+#if CONFIG_BASE_ADDR != 0x1000
+#error "CONFIG_BASE_ADDR should be 0x1000"
+#endif
+
+#if CONFIG_COUNT != 3
+#error "CONFIG_COUNT should be 3"
+#endif
+
 int main(void) { return 0; }
